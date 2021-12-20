471f7b29a7c834b51635f198c304314e15b78e95
yugaro <yugaro.yugaro@gmail.com>
2021-12-20 15:12:37 +0900

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.jsx</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -21,5 +26,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
